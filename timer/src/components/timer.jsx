bf3afff6e3f70f15b7a9789ab14904152a860f44
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-21 09:19:54 +0200

    start

---
+++ timer/src/components/timer.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react"
+
+
+ export function Timer() {
+    const [data, setData] = useState([])
+    const [count, setCount] = useState(0)
+
+
+
+    // useEffect(() => {
+    //     fetch("https://jsonplaceholder.typicode.com/todo/1")
+    //     .then((res) => res.json())
+    //     .then((data) => setData(data));
+      
+    // }, [])
+
+
+    useEffect(() => {
+        if (count === 5) {
+            alert (`Du har klikket 5 gange`)
+        }
+    }, [count])
+
+    return (
+        <>
+        <h1>helllo</h1>
+        <button onClick={() => setCount(prevCount+1)}>Count: {count} </button>
+       </>
+    );
+}
+
